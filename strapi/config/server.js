e94c3007281c26c4796fbbf744326a6dd01213ea
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-10-22 08:48:26 -0300

    feat: ✨ add strapi default settings for postgres

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5a94452175474763fb32c01bc0c696b4'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bae6763d-e24b-4d42-ad49-43753a3a2926'
+};
